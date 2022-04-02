987f726b56603de8f8222eadd03bfff1e0368e22
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2022-04-02 18:24:56 -0300

    Melhora o menu, automatizando os botoes

---
+++ js/components/navbar.js
@@ -0,0 +1,24 @@
+function navbar() {
+  let menu = {
+    'cadastro': 'Cadastrar',
+    'listar': 'Listar frutas',
+    'perfil': 'Meu Perfil',
+    'config': 'Configurações',
+    'report': 'Relatórios',
+    'login': 'Sair',
+  };
+
+  let buttons = '';
+
+  for (let functionName in menu) {
+    buttons += `
+      <button onclick="alterarConteudo('${functionName}')" class="btn btn-primary">
+        ${menu[functionName]}
+      </button>
+    `
+  }
+
+  return buttons;
+}
+
+document.getElementById('navbar').innerHTML = navbar();
