091f996eb633f93476e3eb567c5c9f5b2b914d2c
Andrade <alves.guerra@hotmail.com>
2022-04-09 19:38:15 -0300

    add new type-payment

--- js/pages/paymentType/addPaymentType.js
+++ js/pages/paymentType/addPaymentType.js
@@ -1,13 +1,33 @@
+function savePaymentType() {
+  event.preventDefault();
+
+  let newPaymentTypes = {
+    name: document.getElementById('payment-name').value,
+    description: document.getElementById('payment-description').value, 
+  };
+
+  fetch ('http://localhost:3000/payment-types', {
+    method: 'POST',
+    body: JSON.stringify(newPaymentTypes),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  alert('Ok, PaymentType');
+  document.getElementById('add-PaymentType').reset();
+}
+
 function addPaymentType() {
     return `
       <h2>Tipo de Pagamento</h2>
       <hr>
-      <form>
-        <label for="name-payment">Nome</label>
-        <input id="name" class="form-control mb-3" placeholder="Digite aqui">
+      <form onsubmit="savePaymentType()">
+        <label for="payment-name">Nome</label>
+        <input id="payment-name" class="form-control mb-3" placeholder="Digite aqui">
 
-        <label for="description-payment">Descrição</label>
-        <input id="description" class="form-control mb-3" placeholder="Digite aqui">
+        <label for="payment-description">Descrição</label>
+        <input id="payment-description" class="form-control mb-3" placeholder="Digite aqui">
 
         <button class="btn btn-success">Cadastrar</button>
       </form>
